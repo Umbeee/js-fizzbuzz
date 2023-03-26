6a4a787ce6ea184b3d779902dbbd55fd7dfb6d6c
Umbeee <umberto.nesta@hotmail.com>
2023-03-26 16:45:52 +0200

    esercizio completo

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,25 @@
+
+let box = document.getElementById('box-container');
+
+
 for(let i=1; i<=100; i++){
 
    if(i%3==0){
 
     if(i%5==0){
         console.log('FizzBuzz');
+        box.innerHTML += `<div class="box box--fizzbuzz">FizzBuzz</div>`  
     }else{
         console.log('Fizz');
+        box.innerHTML += `<div class="box box--fizz">Fizz</div>`  
         }
     
     }else if(i%5==0){
         console.log('Buzz');
+        box.innerHTML += `<div class="box box--buzz">Buzz</div>`  
     }else{
         console.log(i);
+        box.innerHTML += `<div class="box box--num">${i}</div>`  
     }
-   
+ 
 }
